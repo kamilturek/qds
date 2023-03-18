70c8c21a04163fba85f1a0ee7c6c647c6fa2a19e
Kamil Turek <kamil.turek@hotmail.com>
2023-03-18 14:12:29 +0100

    Modify lint workflow

--- qds/__init__.py
+++ qds/__init__.py
@@ -12,3 +12,18 @@ from .api import (
     is_display_online,
 )
 from .display import Display
+
+__all__ = (
+    "get_active_display_count",
+    "get_active_display_list",
+    "get_display_origin",
+    "get_display_pixels_high",
+    "get_display_pixels_wide",
+    "get_main_display_id",
+    "get_online_display_count",
+    "get_online_display_list",
+    "is_display_active",
+    "is_display_builtin",
+    "is_display_online",
+    "Display",
+)
